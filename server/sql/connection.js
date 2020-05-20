53f5b8bb6a6d481bf181e5638eddf1f854b2541f
galvanitic <develop@rglvn.com>
2020-05-19 21:57:15 -0500

    Fixed MySQL pooling for Google App Engine.

--- server/sql/connection.js
+++ server/sql/connection.js
@@ -7,13 +7,20 @@ class Connection {
       console.log('ligando a mysql...');
 
       //create pool
-      this.pool = mysql.createPool({
+      const config = mysql.createPool({
         connectionLimit: 100,
         host: galvanite.host,
         user: galvanite.user,
         password: galvanite.password,
         database: galvanite.db
-      })
+      });
+
+      if (process.env.NODE_ENV === 'production' && process.env.CLOUD_INSTANCE) {
+        console.log(`connect socket: ${process.env.CLOUD_INSTANCE}`)
+        config.socketPath = `/cloudsql/${process.env.CLOUD_INSTANCE}`
+      }
+
+      this.pool = mysql.createPool(config)
 
       return this.pool;
     }
